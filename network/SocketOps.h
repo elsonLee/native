32e5e83b6abbc3cb1e7a3cdd42c41b8e5806d9de
lexun <llxmedici@gmail.com>
2018-10-30 15:41:11 +0800

    reincident SocketOps.h

--- network/SocketOps.h
+++ network/SocketOps.h
@@ -6,27 +6,33 @@
 
 namespace sockops
 {
-    inline uint16_t hostToNetwork16 (uint16_t v) {
+    inline uint16_t
+    hostToNetwork16 (uint16_t v) {
         return htobe16(v);
     }
 
-    inline uint32_t hostToNetwork32 (uint32_t v) {
+    inline uint32_t
+    hostToNetwork32 (uint32_t v) {
         return htobe32(v);
     }
 
-    inline uint64_t hostToNetwork64 (uint64_t v) {
+    inline uint64_t
+    hostToNetwork64 (uint64_t v) {
         return htobe64(v);
     }
 
-    inline uint16_t networkToHost16 (uint16_t v) {
+    inline uint16_t
+    networkToHost16 (uint16_t v) {
         return be16toh(v);
     }
 
-    inline uint32_t networkToHost32 (uint32_t v) {
+    inline uint32_t
+    networkToHost32 (uint32_t v) {
         return be32toh(v);
     }
 
-    inline uint64_t networkToHost64 (uint64_t v) {
+    inline uint64_t
+    networkToHost64 (uint64_t v) {
         return be64toh(v);
     }
 
